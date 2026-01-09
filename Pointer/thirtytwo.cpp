90a3d2d79d1038ef015c68da20c43c69f1a43c92
Mohammad Mustafa <mustafamohd7636@gmail.com>
2026-01-09 23:41:34 +0530

    commit

---
+++ Pointer/thirtytwo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    cout << *arr << endl;
+    cout << *(arr+1) << endl;
+    cout << *(arr+2) << endl;
+    cout << *(arr+3) << endl;
+    cout << *(arr+4) << endl;
+    return 0;
+}
